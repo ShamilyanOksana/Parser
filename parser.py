05dcf604ff14d8ff60f4c8cdb619bd9c540dfa3c
ShamilyanOksana <shamilyanoksana@gmail.com>
2017-12-27 01:25:25 +0300

    класс Phone, сортировка цен
    сортировка по возрастанию
    метод sort()

--- parser.py
+++ parser.py
@@ -2,6 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 
+class Phone:
+
+    pass
+
+
 def get_html(url):
     url = "https://www.avito.ru/taganrog/telefony/samsung?q=sumsung&p=1"
     r = requests.get(url)
@@ -17,22 +22,61 @@ def get_total_pages(html):
     return int(total_pages)
 
 
+def print_information(all_info):
+    all_info.sort(key=lambda phone: phone.price)
+    for info in all_info:
+        print(info.title)
+        print(info.url)
+        print(info.price)
+        print(info.currency)
+
+
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
 
-    ads = soup.find('div', class_='catalog-list').find_all('h3', class_='title item-description-title')
+    ads = soup.find('div', class_='catalog-list').find_all('div', class_='description')
+    count = 0
+    for ad in ads:
+        all_info.append(Phone())
+        all_info[count].title = get_title(ad)
+        all_info[count].url = get_link(ad)
+        pre_price = get_price(ad)
+        all_info[count].price = pre_price[0]
+        all_info[count].currency = pre_price[1]
+        count+=1
+    return all_info
 
-    for j in range(len(ads)):
-        print(j+1, '.', end=" ")
-        current_ads = ads[j].find('a', class_='item-description-title-link')
-        title = current_ads.get('title')
-        print(title)
 
-        link = "https://www.avito.ru" + current_ads.get('href')
-        print(link)
+def get_title(current_ads):
+    try:
+        title = current_ads.find('a', class_='item-description-title-link').get('title')
+        return title
+    except Exception:
+        pass
 
-        price = current_ads.get('')
 
+def get_link(current_ads):
+    try:
+        link = "https://www.avito.ru" + current_ads.find('a', class_='item-description-title-link').get('href')
+        return link
+    except Exception:
+        pass
+
+
+def get_price(current_ads):
+
+    try:
+        price = current_ads.find('div', class_='about').text.split(' ')[2:]
+        if price[0].isdigit() and price[1].isdigit():
+            currency = price[2]
+            price = int(price[0])*1000 + int(price[1])
+
+        else:
+            currency = price[1]
+            price = int(price[0])
+        return [price, currency]
+    except Exception:
+        pass
 
 
 def main():
@@ -48,13 +92,10 @@ def main():
     for i in range(1, 2):
         url_gen = base_url + page_part + str(i) + query_part
         html = get_html(url_gen)
-        print(url_gen)
-        get_page_data(html)
+        all_info = get_page_data(html)
+        print_information(all_info)
 
+all_info = []
 
 if __name__ == "__main__":
     main()
-
-
-
-
